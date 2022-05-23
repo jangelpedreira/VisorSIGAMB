b535deb3251a66cf0f7ea439bb3d40cc0e6c0a2f
jangelpedreira <jangel.pedreira@gmail.com>
2022-05-23 12:19:19 +0200

    Commit

--- init.js
+++ init.js
@@ -106,7 +106,7 @@ var ie = (function() {
       // window.path + 'libs/goldenlayout/goldenlayout-light-theme.css'
     ]);
 
-    console.log("WINDOW.APIURL: " + window.apiUrl)
+    console.log("***WINDOW.APIURL***: " + window.apiUrl)
 
     if (window.apiUrl.substr(window.apiUrl.length - 'arcgis-js-api/'.length,
       'arcgis-js-api/'.length) === 'arcgis-js-api/') {
